7762ef568a65569e904ff09b08f1463f6954e409
Larissa Corrêa <larimscorrea@gmail.com>
2024-08-28 11:21:34 -0300

    feat: erro 401, mas o erro 500 foi resolvido

--- src/controllers/user.py
+++ src/controllers/user.py
@@ -2,6 +2,7 @@ from flask import Blueprint, request
 from src.app import User, db
 from http import HTTPStatus
 from sqlalchemy import inspect
+from flask_jwt_extended import jwt_required
 
 app = Blueprint('user', __name__, url_prefix='/users')
 
@@ -23,12 +24,13 @@ def _list_users():
     ]
 
 @app.route('/', methods=['GET', 'POST'])
+@jwt_required()
 def list_or_create_user():
     if request.method == 'POST':
         _create_user()
-        return {'message': 'User created'}, HTTPStatus.CREATED
+        return {"message": "User created"}, HTTPStatus.CREATED
     else: 
-        return {'users': _list_users()}
+        return {"identity": get_jwt_identity(), "users": _list_users()}
 
 @app.route('/<int:user_id>', methods=['GET'])
 def get_user(user_id): 
