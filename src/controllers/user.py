1a34008af9edf4cba6abce35722c9a9aa38fa7d6
Larissa Corrêa <larimscorrea@gmail.com>
2024-08-29 10:36:21 -0300

    feat: código funcionando, autenticação feita

--- src/controllers/user.py
+++ src/controllers/user.py
@@ -1,8 +1,12 @@
-from flask import Blueprint, request
+from flask import Flask, Blueprint, request
 from src.app import User, db
 from http import HTTPStatus
 from sqlalchemy import inspect
-from flask_jwt_extended import jwt_required
+from flask_jwt_extended import jwt_required, get_jwt_identity, JWTManager
+
+app = Flask(__name__)
+app.config['JWT_SECRET_KEY'] = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJmcmVzaCI6ZmFsc2UsImlhdCI6MTcyNDkzNzg3MywianRpIjoiOTljZDQzYzctNGExZi00ZDQ5LWExNDItOTE2OWRmZTYwMjI2IiwidHlwZSI6ImFjY2VzcyIsInN1YiI6InRlc3QiLCJuYmYiOjE3MjQ5Mzc4NzMsImNzcmYiOiI3NjIyMGNhNS0wMjJkLTQ4NzktYmVlNS04ZjZmYmZmMDczZmYiLCJleHAiOjE3MjQ5Mzg3NzN9.z85Sp_1ChjsWpXKjvy7bJThwnGfENgchn2YCB2u4jy8'  # Substitua pelo seu JWT secret
+jwt = JWTManager(app)
 
 app = Blueprint('user', __name__, url_prefix='/users')
 
@@ -45,7 +49,6 @@ def update_user(user_id):
     user = db.get_or_404(User, user_id)
     data = request.json
 
-    # Certifique-se de que JobStatus é relevante. Caso contrário, mude para User.
     mapper = inspect(User)  # Ou JobStatus, se isso for necessário.
     for column in mapper.attrs:
         if column.key in data: 
